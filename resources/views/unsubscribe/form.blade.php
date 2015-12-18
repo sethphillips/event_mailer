f7601f68066756e09a4e262cdb0779c0afc5ad1f
Seth Phillips <seth.phillips@trms.com>
2015-12-17 22:09:04 -0600

    tweak unsubscribe page

--- resources/views/unsubscribe/form.blade.php
+++ resources/views/unsubscribe/form.blade.php
@@ -1,31 +1,41 @@
-@extends('layouts.index')
-
-
-@section('content')
-
-
-<div class="container">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Unsubscribe</title>
+	<link rel="stylesheet" href="{!! elixir('dist/app.css') !!}">
+    <script src="{!! elixir('dist/app.js') !!}"></script>
+</head>
+<body>
 	
-	<div class="left bg"></div>
-	<div class="right bg"></div>
+
+
+<div class="container unsubscribe">
+
 	
-	<div class="title-bar">
-		<div class="logo">
-			<a href="http://www.exhibitpartners.com" target="_blank"><img src="{!! asset('img/ep_logo.jpg') !!}" alt="EP_Logo" ></a>
+	<div class="row">
+		<div class="col-sm-2 pull-left logo">
+			<a href="http://www.exhibitpartners.com" target="_blank"><img src="{!! asset('img/ep_logo.jpg') !!}" alt="EP_Logo" class="img-responsive img"></a>
 		</div>
+
 	</div>
-	<div class="content two">
-		@if( isset($message) )
-			<h1>{{ $message }}</h1>
-		@endif
-		<form action="{{ URL::route('unsubscribe.submit') }}" method="post" class='form buttons'>
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-			<input type="email" name="email" class="form-control" placeholder="enter your email">
-			<input type="submit" value="Unsubscribe" class="btn">
-		</form>
+	<div class="row">
+		<div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+			@if( isset($message) )
+				<h1>{{ $message }}</h1>
+			@endif
+			<form action="{{ URL::route('unsubscribe.submit') }}" method="post" class='form'>
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				<div class="form-group">
+					<input type="email" name="email" class="form-control" placeholder="enter your email">
+				</div>
+				<input type="submit" value="Unsubscribe" class="btn">
+			</form>
+		</div>
 	</div>
 </div>
 
 
 
-@stop
+</body>
+</html>
--- resources/views/unsubscribe/response.blade.php
+++ resources/views/unsubscribe/response.blade.php
@@ -1,24 +1,33 @@
-@extends('layouts.index')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Unsubscribe</title>
+	<link rel="stylesheet" href="{!! elixir('dist/app.css') !!}">
+    <script src="{!! elixir('dist/app.js') !!}"></script>
+
+</head>
+<body>
 
 
-@section('content')
 
+<div class="container unsubscribe">
 
-<div class="container">
 	
-	<div class="left bg"></div>
-	<div class="right bg"></div>
-	
-	<div class="title-bar">
-		<div class="logo">
-			<a href="http://www.exhibitpartners.com" target="_blank"><img src="{!! asset('img/ep_logo.jpg') !!}" alt="EP_Logo" ></a>
+	<div class="row">
+		<div class="col-sm-2 pull-left logo">
+			<a href="http://www.exhibitpartners.com" target="_blank"><img src="{!! asset('img/ep_logo.jpg') !!}" alt="EP_Logo" class="img-responsive img"></a>
 		</div>
+
 	</div>
-	<div class="content two">
-		<h1>{{ $message }}</h1>
+	<div class="row">
+		<div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+			<h1>{{ $message }}</h1>
+		</div>
 	</div>
 </div>
 
 
 
-@stop
+</body>
+</html>
